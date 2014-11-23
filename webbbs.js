c0938475a0c150508231c99a70a7b56c570411a3
Mithgol the Webmaster <getgit@mithgol.ru>
2014-11-23 20:16:20 +0300

    configure the `express-handlebars` module as the view engine

--- webbbs.js
+++ webbbs.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var path = require('path');
 
 module.exports = function(/* options */){
    var app = express();
+   var exhandler = require('express-handlebars'); // tribute to 「Asura Cryin'」
+
+   // Handlebars!
+   var dirNodeViews = path.join(__dirname, 'node_views');
+   app.engine('handlebars', exhandler({
+      defaultLayout: 'default',
+      layoutsDir:   path.join(dirNodeViews, 'layouts'),
+      partialsDir:  path.join(dirNodeViews, 'partials')
+   }));
+   app.set('views', dirNodeViews);
+   app.set('view engine', 'handlebars');
 
    app.get('/', function(req, res){
       /*jshint unused:vars */
